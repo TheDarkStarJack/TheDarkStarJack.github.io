dae80ba0cb5f1fe696d32f4e24e0d912b222a6bc
TheDarkStarJack <2403220952@qq.com>
2025-03-16 11:02:33 +0800

    2025-03-16 11:02:31

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741883945',resources: [
+    cacheName: 'chirpy-1742094153',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
